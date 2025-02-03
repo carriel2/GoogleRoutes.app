474bce589e68b213528e17595375d8d413e146eb
Carriel <pcarriel05@gmail.com>
2025-02-03 11:43:23 -0300

    visual-implementations

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,7 +6,7 @@ import '../utils/maps_launcher.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
-  
+
   @override
   HomePageState createState() => HomePageState();
 }
@@ -14,14 +14,14 @@ class HomePage extends StatefulWidget {
 class HomePageState extends State<HomePage> {
   List<TextEditingController> _enderecosControllers = [];
   String? _localizacaoAtual;
-  
+
   @override
   void initState() {
     super.initState();
     _enderecosControllers = List.generate(2, (_) => TextEditingController());
     _obterLocalizacaoAtual();
   }
-  
+
   @override
   void dispose() {
     for (var controller in _enderecosControllers) {
@@ -29,24 +29,31 @@ class HomePageState extends State<HomePage> {
     }
     super.dispose();
   }
-  
+
   Future<void> _obterLocalizacaoAtual() async {
     try {
-      Position posicao = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
-      List<Placemark> placemarks = await placemarkFromCoordinates(posicao.latitude, posicao.longitude);
+      Position posicao = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+        posicao.latitude,
+        posicao.longitude,
+      );
+
       Placemark local = placemarks.first;
-      String endereco = "${local.street}, ${local.subLocality}, ${local.locality}, ${local.administrativeArea}";
-      
+      String endereco =
+          "${local.street}, ${local.subLocality}, ${local.locality}, ${local.administrativeArea}";
+
       setState(() {
         _localizacaoAtual = endereco;
       });
-      
       print("Localização Atual: $_localizacaoAtual");
     } catch (e) {
       print("Erro ao obter localização: $e");
     }
   }
-  
+
   void buscarRota() async {
     if (_localizacaoAtual == null) {
       ScaffoldMessenger.of(context).showSnackBar(
@@ -54,29 +61,28 @@ class HomePageState extends State<HomePage> {
       );
       return;
     }
-    
+
     List<String> enderecos = _enderecosControllers
         .map((controller) => controller.text.trim())
         .where((text) => text.isNotEmpty)
         .toList();
-    
+
     if (enderecos.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Insira pelo menos um destino!")),
       );
       return;
     }
-    
+
     String origem = _localizacaoAtual!;
     print("Endereços enviados para API: $enderecos");
-    
+
     try {
-      // Envia um JSON contendo 'origem' e 'enderecos' para o backend.
       Map<String, dynamic> requestData = {
         'origem': origem,
         'enderecos': enderecos,
       };
-      
+
       List<dynamic> rotaOtimizada = await ApiService.otimizarRota(requestData);
       abrirGoogleMaps(rotaOtimizada.cast<String>());
     } catch (e) {
@@ -86,13 +92,13 @@ class HomePageState extends State<HomePage> {
       );
     }
   }
-  
+
   void adicionarCampo() {
     setState(() {
       _enderecosControllers.add(TextEditingController());
     });
   }
-  
+
   void removerCampo(int index) {
     if (_enderecosControllers.length > 1) {
       setState(() {
@@ -101,58 +107,121 @@ class HomePageState extends State<HomePage> {
       });
     }
   }
-  
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Otimização de Rotas")),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              _localizacaoAtual != null ? "Localização Atual: $_localizacaoAtual" : "Obtendo localização...",
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Expanded(
-              child: ListView.builder(
-                itemCount: _enderecosControllers.length,
-                itemBuilder: (context, index) {
-                  return Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Otimização de Rotas",
+            style: TextStyle(color: Colors.white),
+          ),
+          backgroundColor: Colors.black,
+          centerTitle: true,
+        ),
+        backgroundColor: Colors.grey[200],
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Exibe a localização atual em um Card
+              Card(
+                elevation: 4,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Text(
+                    _localizacaoAtual != null
+                        ? "Localização Atual: $_localizacaoAtual"
+                        : "Obtendo localização...",
+                    style: const TextStyle(
+                        fontSize: 16, fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              const Text(
+                "Insira os endereços:",
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                child: ListView.separated(
+                  itemCount: _enderecosControllers.length,
+                  separatorBuilder: (context, index) => SizedBox(height: 8),
+                  itemBuilder: (context, index) {
+                    return Card(
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8)),
+                      child: ListTile(
+                        leading:
+                            const Icon(Icons.location_on, color: Colors.black),
+                        title: TextField(
                           controller: _enderecosControllers[index],
                           decoration: InputDecoration(
                             labelText: "Endereço ${index + 1}",
-                            border: const OutlineInputBorder(),
+                            labelStyle: TextStyle(
+                                color: Colors.black), // Mantém o texto preto
+                            border: InputBorder.none,
+                            contentPadding: const EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 14),
                           ),
                         ),
+                        trailing: _enderecosControllers.length > 1
+                            ? IconButton(
+                                icon: const Icon(Icons.remove_circle,
+                                    color: Colors.red),
+                                onPressed: () => removerCampo(index),
+                              )
+                            : null,
                       ),
-                      if (_enderecosControllers.length > 1)
-                        IconButton(
-                          icon: const Icon(Icons.remove_circle, color: Colors.red),
-                          onPressed: () => removerCampo(index),
-                        ),
-                    ],
-                  );
-                },
+                    );
+                  },
+                ),
               ),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton.icon(
-              onPressed: adicionarCampo,
-              icon: const Icon(Icons.add),
-              label: const Text("Adicionar Endereço"),
-            ),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: buscarRota,
-              child: const Text("Buscar Rota"),
-            ),
-          ],
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 12),
+                      textStyle: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: adicionarCampo,
+                    icon: const Icon(Icons.add, color: Colors.white),
+                    label: const Text(
+                      "Adicionar Endereço",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 36, vertical: 12),
+                      textStyle: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    onPressed: buscarRota,
+                    child: const Text(
+                      "Buscar Rota",
+                      style: TextStyle(color: Colors.black),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
